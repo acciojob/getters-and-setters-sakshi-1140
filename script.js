8649b4420dbbe7d6f6611100b2dd4804088faf67
Sakshi Choudhary <103510144+sakshi-1140@users.noreply.github.com>
2024-06-19 22:57:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 //complete this code
 class Person {
 	constructor(name,age){
-    this.n=name
-    this.a=age
+    this._name=name
+    this._age=age
   }
   
   get name(){
-    return this.n
+    return this._name
   }
   
   set age(age){
-    this.a=age
+    this._age=age
   }
 }
 
